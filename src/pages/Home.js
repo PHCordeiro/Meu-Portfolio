e273e57461de487cbcb0527fe15d9770c2a40e7a
PHCordeiro <phseabracordeiro@gmail.com>
2024-12-24 09:47:45 -0300

    Header Atualizado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,8 @@ function Home() {
   };
 
   return (
-    <div className="home-container">
+    <>
+      {/* Header */}
       <header className="header">
         <div className="button-container">
           <Link to="/sobre">
@@ -26,41 +27,47 @@ function Home() {
         </div>
       </header>
 
-      <img
-        src={`${process.env.PUBLIC_URL}/eu.jpg`}
-        alt="Foto de Pedro"
-        className="profile-picture"
-      />
+      {/* Home Container */}
+      <div className="home-container">
+        <img
+          src={`${process.env.PUBLIC_URL}/eu.jpg`}
+          alt="Foto de Pedro"
+          className="profile-picture"
+        />
 
-      <div className="character-info">
-        <h2>{language === 'pt' ? 'Ficha do Personagem' : 'Character Profile'}</h2>
-        <p><strong>{language === 'pt' ? 'Nome:' : 'Name:'}</strong> Pedro Henrique de Souza Seabra Mattos Cordeiro</p>
-        <p><strong>{language === 'pt' ? 'Profissão:' : 'Profession:'}</strong> {language === 'pt' ? 'Engenheiro da Computação / Programador de Jogos' : 'Computer Engineer / Game Developer'}</p>
-        <p><strong>{language === 'pt' ? 'Descrição:' : 'Description:'}</strong> {language === 'pt' ? 'Um profissional entusiasmado em busca de conhecimento e crescimento contínuo.' : 'An enthusiastic professional seeking knowledge and continuous growth.'}</p>
-        <p><strong>{language === 'pt' ? 'Idade:' : 'Age:'}</strong> 20 {language === 'pt' ? 'anos' : 'years old'}</p>
-      </div>
+        <div className="character-info">
+          <h2>{language === 'pt' ? 'Ficha do Personagem' : 'Character Profile'}</h2>
+          <p>
+            <strong>{language === 'pt' ? 'Nome:' : 'Name:'}</strong> Pedro Henrique de Souza Seabra Mattos Cordeiro
+          </p>
+          <p>
+            <strong>{language === 'pt' ? 'Profissão:' : 'Profession:'}</strong>{' '}
+            {language === 'pt' ? 'Engenheiro da Computação / Programador de Jogos' : 'Computer Engineer / Game Developer'}
+          </p>
+          <p>
+            <strong>{language === 'pt' ? 'Descrição:' : 'Description:'}</strong>{' '}
+            {language === 'pt' ? 'Um profissional entusiasmado em busca de conhecimento e crescimento contínuo.' : 'An enthusiastic professional seeking knowledge and continuous growth.'}
+          </p>
+          <p>
+            <strong>{language === 'pt' ? 'Idade:' : 'Age:'}</strong> 20 {language === 'pt' ? 'anos' : 'years old'}
+          </p>
+        </div>
 
-      <div className="projects-section">
-        <Link to="/projetos-jogos" className="central-button">
-          <button>
-            {language === 'pt' ? 'Projetos de Jogos' : 'Game Projects'}
-          </button>
-        </Link>
-        <Link to="/projetos-programas" className="central-button">
-          <button>
-            {language === 'pt' ? 'Programas' : 'Programs'}
-          </button>
-        </Link>
+        <div className="projects-section">
+          <Link to="/projetos-jogos" className="central-button">
+            <button>{language === 'pt' ? 'Projetos de Jogos' : 'Game Projects'}</button>
+          </Link>
+          <Link to="/projetos-programas" className="central-button">
+            <button>{language === 'pt' ? 'Programas' : 'Programs'}</button>
+          </Link>
+        </div>
+        <div className="carreira">
+          <Link to="/Experiencias/Experiencias" className="central-button">
+            <button>{language === 'pt' ? 'Experiências' : 'Experiences'}</button>
+          </Link>
+        </div>
       </div>
-
-      <div className="carreira">
-      <Link to="/Experiencias/Experiencias" className="central-button">
-          <button>
-            {language === 'pt' ? 'Experiências' : 'Experiences'}
-          </button>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
 
